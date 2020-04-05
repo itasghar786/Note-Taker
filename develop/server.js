88d236a270cf4fa9873af89a4b21809181246108
asghar sheikh <itasghar786@gmail.com>
2020-04-05 15:02:28 -0400

    Working on paths for folders

--- develop/server.js
+++ develop/server.js
@@ -4,7 +4,7 @@ let app = express();
 const fs = require('fs');
 
 
-let noteData = require ('./develop/db/db.json');
+let noteData = require ('Note-Taker/develop/db/db.json');
 const PORT = process.env.PORT || 4040;
 
 
@@ -20,29 +20,29 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-  app.get("/notes", function(req, res) {
+  app.get(".develop/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
   });
 
-  app.get("/api/notes", function(req, res) {
+  app.get("./api/notes", function(req, res) {
     return res.json(noteData);
   });
-
+  
   app.post('/api/notes', (req, res) => {
     noteData.push(req.body);
     noteData.forEach((note, i) => {
         note.id = i + 1;
     })
     let newNote = JSON.stringify(noteData);
-    fs.writeFileSync('./develop/db/db.json', newNote);
-
+    fs.writeFileSync('Note-Taker/develop/db/db.json', newNote);
+   
     res.json(noteData);
 })
 
 app.delete('/api/notes/:id', (req,res)=>{
   let filtered= noteData.filter(note=>note.id !==parseInt(req.params.id));
   console.log(filtered)
-  fs.writeFileSync('./develop/db/db.json', JSON.stringify(filtered))
+  fs.writeFileSync('Note-Taker/develop/db/db.json', JSON.stringify(filtered))
 
   res.send('localhost:4040/notes');
 })
